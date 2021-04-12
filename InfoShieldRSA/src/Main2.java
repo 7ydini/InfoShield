fb867dce0ac9e88c97e95444499ee6ad5cebb0d2
7ydini <61342487+7ydini@users.noreply.github.com>
2021-04-12 09:44:35 +0300

    Update Main2.java

--- InfoShieldRSA/src/Main2.java
+++ InfoShieldRSA/src/Main2.java
@@ -7,10 +7,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int p, q, n, m, d = 0, e = 0;
+        char[] word = new char[] {
+                'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+                'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т',
+                'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
+                'Э', 'Ю', 'Я', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+                'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+                'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ',
+                '0', '1', '2', '3', '4', '5','6', '7', '8', '9'
+        };
+
 
         String message = "";
         Scanner scan = new Scanner(System.in);
         //Ввод чисел с клавиатуры через сканер.
+        //System.out.print("Ручной/авто?(1/2) >>");
      do{
         System.out.println("Введите простые числа!\n");
         System.out.print("P = ");
@@ -18,37 +29,48 @@ public class Main {
         System.out.print("Q = ");
         q = scan.nextInt();
      }while(!simpleNum(p) || !simpleNum(q));
-     
      n = p * q;
      System.out.println("N = " + n);
      //Вычисляем m
      m = (p - 1) * (q - 1);
+     System.out.println("M = " + m);
      //Ищем случайное простое число d
-     while(!simpleNum(d)) {
-         d = (int) (1 + Math.random() * 100);
-     }
-       /* d = m - 1;
+//     while(!simpleNum(d)) {
+//        d = (int) (2 + Math.random() * 100);
+//     }
+        d = m - 1;
         for(int i = 2; i <= m; i++) {
            if (m % i == 0 && d % i == 0) {
                 d--;
-               i = 1;
+               i = 2;
            }
-          }*/
-
+          }
      System.out.println("D = " + d);
      //Ищем e;
+        e = 10;
         while(!((e * d)%(m) == 1)){
-            e = (int) (Math.random() * 1000);
+            e++;
         }
+
         System.out.println("E = " + e);
+        System.out.println("M = " + m);
         System.out.println("E = " + e + "; and N =" + n + "; open key!");
         System.out.println("D = " + d + "; and N =" + n + "; close key!");
-        while (message.length() == 0){
-            System.out.println("Введите сообщение на русском>>");
+        try {
+            File key = new File("src/key.txt");
+            FileWriter keyWrite = new FileWriter(key);
+            String keyMess = "E = " + e + "; M = " + m + "; D = " + d + "; N = " + n;
+            keyWrite.write(keyMess);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
+
+        while (message.strip().length() == 0){
+            System.out.println("Введите сообщение >>");
             message = scan.nextLine();
         }
         scan.close();
-        //while (message.strip().length() == 0);
         message = message.toUpperCase();//Переводим все буквы в верний регистр
         try{
             File mess = new File("src/Message.txt");
@@ -61,11 +83,11 @@ public class Main {
             exception.printStackTrace();
         }
         System.out.println("Шифруем сообщение...");
-        Encrypt(message, e, n);
+        Encrypt(message, e, n, word);
         System.out.println("Сообщение успешно зашифровано!");
 
         System.out.println("Расшифровываем сообщение...");
-        Decrypt(d, n);
+        Decrypt(d, n, word);
         System.out.println("Сообщение успешно Расшифровано!");
     }
     public static boolean simpleNum(int num){// Метод проверяющий число, простое ли оно.
@@ -79,15 +101,7 @@ public class Main {
         }
         return true;
     }
-    public static void Encrypt(String mess, int e, int n){
-
-            char[] word = new char[] {
-                    'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
-                    'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т',
-                    'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
-                    'Э', 'Ю', 'Я', ' ', '0', '1', '2', '3', '4', '5',
-                    '6', '7', '8', '9'
-            };
+    public static void Encrypt(String mess, int e, int n, char[] word){
             String encMess = "";
             int index = -1;
             char[] messArr = mess.toCharArray();
@@ -110,39 +124,30 @@ public class Main {
 
 
     }
-    public static void Decrypt( int d, int n){
+    public static void Decrypt( int d, int n, char[] word){
 
-        char[] word = new char[] {
-                'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
-                'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т',
-                'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
-                'Э', 'Ю', 'Я', ' ', '0', '1', '2', '3', '4', '5',
-                '6', '7', '8', '9'
-        };
+
         try {
             File enc = new File("src/encrypt.txt");
-            FileReader encRead = new FileReader(enc);
-            //создаем BufferedReader с существующего FileReader для построчного считывания
-            BufferedReader reader = new BufferedReader(encRead);
+            FileReader encrypt = new FileReader(enc);
+            BufferedReader encRead = new BufferedReader(encrypt);
             File dec = new File("src/decrypt.txt");
             FileWriter decWrite = new FileWriter(dec);
+            String encMess = "";
             String message = "";
             String decrypt = "";
-            int index = 0;
-            String encMess = String.valueOf(reader.readLine());
-            System.out.println("message :" + encMess);
-            System.out.println(message);
-            int[] messageNumb = Arrays.stream(encMess.split(" ")).mapToInt(Integer::parseInt).toArray();
+            encMess = String.valueOf(encRead.readLine());
+            message = encMess;
+            //System.out.println(encMess);
+            int[] messageNumb = Arrays.stream(message.split(" ")).mapToInt(Integer::parseInt).toArray();
             for (int i : messageNumb) {
                 System.out.println(i);
-                index = (i ^ d) % n;
-                decWrite.append(word[index]);
-                decWrite.append(" ");
-                //decrypt = decrypt + String.valueOf(word[index]) + " ";
+                decrypt = decrypt + String.valueOf(word[(i ^ d) % n]);
             }
-            reader.close();
+            decWrite.write(decrypt);
             decWrite.close();
             encRead.close();
+            encrypt.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
